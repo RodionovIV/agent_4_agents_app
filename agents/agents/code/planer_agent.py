28113a401774d41414e0628429cfdf00155e5897
RodionovIV <mr.ts777@yandex.ru>
2025-06-22 17:44:58 +0300

    refactor planner_agent

--- agents/agents/code/planer_agent.py
+++ agents/agents/code/planer_agent.py
@@ -7,8 +7,11 @@ from langchain.schema import HumanMessage
 
 from langgraph.checkpoint.memory import MemorySaver
 
-import re
 from typing import TypedDict, List
+
+from abstract.abstract_agent import AbstractAgent
+from agents.utils.text_formatter import TextFormatter
+from agents.utils.result_formatter import ResultFormatter
 
 _LOGGER = customLogger.getLogger(__name__)
 POSTFIX = "\n\nИсправь, пожалуйста, и сгенерируй план разработки полностью заново."
@@ -20,22 +23,23 @@ class PlAgentState(TypedDict):
     messages: List
 
 
-class PlAgent:
+class PlAgent(AbstractAgent):
     def __init__(self):
         pass
 
-    async def create(self):
+    async def create_agent(self):
         tools = await client.get_tools(server_name="planer")
         llm_with_functions = llm.bind_functions(tools)
         self.agent = create_react_agent(
             llm_with_functions, tools, checkpointer=MemorySaver()
         )
 
-    async def run_qa_agent(self, state: PlAgentState, config: dict):
+    async def run_agent(self, state: PlAgentState, config: dict):
+        class_name = self.__class__.__name__.lower()
         _LOGGER.info(
-            f"Status: planner_agent, thread_id: {config['configurable']['thread_id']}"
+            f"Status: {class_name}, thread_id: {config['configurable']['thread_id']}"
         )
-        # state["questions"] = ""
+
         if "messages" in state and state["messages"]:
             old_messages = state["messages"]
             request = state["messages"][-1].content + POSTFIX
@@ -43,31 +47,22 @@ class PlAgent:
         else:
             old_messages = []
             request = state["task"]
-        _LOGGER.info(f"PL REQUEST: {request}")
-        request = {"messages": [HumanMessage(content=request)]}
+
+        _LOGGER.info(f"{class_name}_request: {request}")
+
+        request = TextFormatter.agent_request(request)
+
         response = await self.agent.ainvoke(request, config=config)
         if isinstance(response, dict):
             result = response["messages"][-1].content
         else:
             result = response
-        _LOGGER.info(f"PL RESPONSE: {result}")
-        matches = re.findall(r"\[ВОПРОС\](.*?)\[/ВОПРОС\]", result, re.DOTALL)
-        if matches:
-            ques_string = [f"{i + 1}. {s}" for i, s in enumerate(matches)]
-            state["questions"] = "Возникли вопросы:\n" + "\n".join(ques_string)
-        else:
-            state["result"] = result
+
+        _LOGGER.info(f"{class_name}_response: {result}")
+        state["result"] = result
         state["messages"] = old_messages + [
             HumanMessage(content=result, name="Аналитик")
         ]
-        return state
-
-    def add_message(self, state: PlAgentState, msg: str):
-        if "messages" in state and state["messages"]:
-            old_messages = state["messages"]
-        else:
-            old_messages = []
-        state["messages"] = old_messages + [HumanMessage(content=msg)]
         return state
 
     def get_result(self, state: PlAgentState):
@@ -80,11 +75,11 @@ class PlAgent:
 
     async def run(self, msg: str, state: PlAgentState, config: dict):
         if "messages" not in state or not state["messages"]:
-            await self.create()
+            await self.create_agent()
             state["task"] = pl_prompt.format(system_requirements=state["task"])
         else:
-            state = self.add_message(state, msg)
-        state = await self.run_qa_agent(state, config)
-        response = self.get_result(state)
+            state = TextFormatter.add_message(state, msg)
+        state = await self.run_agent(state, config)
+        response = ResultFormatter.get_result(state)
         response["state"] = state
         return response
